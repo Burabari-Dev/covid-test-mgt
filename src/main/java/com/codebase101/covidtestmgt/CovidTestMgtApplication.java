8ebe525a6da6399515290bf4c02ee4cc4a189fa4
Burabari <burabaridev@gmail.com>
2024-04-05 11:35:02 +0100

    InitCredentials initialized at startup

--- src/main/java/com/codebase101/covidtestmgt/CovidTestMgtApplication.java
+++ src/main/java/com/codebase101/covidtestmgt/CovidTestMgtApplication.java
@@ -3,10 +3,13 @@ package com.codebase101.covidtestmgt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.codebase101.covidtestmgt.config.InitCredentials;
+
 @SpringBootApplication
 public class CovidTestMgtApplication {
 
 	public static void main(String[] args) {
+		InitCredentials.newInstance();					//-> Initialize the singleton class for all credentials
 		SpringApplication.run(CovidTestMgtApplication.class, args);
 	}
 
