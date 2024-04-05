1e0bbe5b6dd4e3a79830db71ae16577431924158
Burabari <burabaridev@gmail.com>
2024-04-05 03:45:15 +0100

    change Creds class to a record

--- src/main/java/com/codebase101/covidtestmgt/pojos/Creds.java
+++ src/main/java/com/codebase101/covidtestmgt/pojos/Creds.java
@@ -1,24 +1,4 @@
 package com.codebase101.covidtestmgt.pojos;
 
-public class Creds {
-  private String email;
-  private String password;
-
-  public String getEmail() {
-    return email;
-  }
-  public void setEmail(String email) {
-    this.email = email;
-  }
-  public String getPassword() {
-    return password;
-  }
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  @Override
-  public String toString() {
-    return email;
-  }
-}
+// record TokenOb(String accessToken){}
+public record Creds(String username, String password, String email){}
